e26b12e1a9884bcfd779d4d918a1619d2b093b61
Pictar <t.raballand@gmail.com>
2014-05-04 15:05:27 +0200

    multilangue

--- 206neutrinos/206neutrinos.py
+++ 206neutrinos/206neutrinos.py
@@ -28,14 +28,21 @@ def             arithmetic_average(user_input):
     res = ((arith_avg * nbr_mesures) + user_input) / (nbr_mesures + 1)
     return res
 
-def             print_data(ec_type, arith_avg, quadra_avg, harmo_avg):
-    print("\tantal malinder :\t%d" % nbr_mesures)
-    print("\tstandardafvilgelse :\t%.2f" % ec_type)
-    print("\taritmetisk gennemsnit :\t%.2f" % arith_avg)
-    print("\tkvadratisk gennemsnit :\t%.2f" % quadra_avg)
-    print("\tharmonisk gennemsnit :\t%.2f\n" % harmo_avg)
+def             print_data(langage):
+    if langage == 'dk':
+        print("\tantal malinder :\t%d" % nbr_mesures)
+        print("\tstandardafvilgelse :\t%.2f" % ec_type)
+        print("\taritmetisk gennemsnit :\t%.2f" % arith_avg)
+        print("\tkvadratisk gennemsnit :\t%.2f" % quadra_avg)
+        print("\tharmonisk gennemsnit :\t%.2f\n" % harmo_avg)
+    elif langage == 'fr':
+        print("\tNombre de mesures :\t%d" % nbr_mesures)
+        print("\tEcart-type :\t\t%.2f" % ec_type)
+        print("\tMoyenne arithmétique :\t%.2f" % arith_avg)
+        print("\tMoyenne quadratique :\t%.2f" % quadra_avg)
+        print("\tMoyenne harmonique :\t%.2f\n" % harmo_avg)
 
-def             exec_prog(user_input):
+def             exec_prog(user_input, langage):
     global new_arith_avg, ec_type, arith_avg, quadra_avg, harmo_avg, nbr_mesures
     new_arith_avg = arithmetic_average(user_input)
     quadra_avg = quadratic_average(user_input)
@@ -43,33 +50,47 @@ def             exec_prog(user_input):
     arith_avg = new_arith_avg
     harmo_avg = harmonic_average(user_input)
     nbr_mesures += 1
-    print_data(ec_type, arith_avg, quadra_avg, harmo_avg)
+    print_data(langage)
     return 0
 
-def             init():
+def             init(langage):
     if nbr_mesures <= 0 or arith_avg <= 0 or harmo_avg <= 0 or ec_type <= 0:
-        print("Error : all the parameters must be > 0")
+        if langage == 'fr':
+            print("Erreur : tous les paramètres doivent être > 0")
+        if langage == 'dk':
+            print("Fejl: alle parametre skal være> 0")
         return 0
     end_loop = 0
     while end_loop == 0:
-        user_input = input("indtast din vaerdi : ")
-        if user_input == 'ende':
+        if langage == 'fr':
+            user_input = input("entrez votre valeur : ")
+        elif langage == 'dk':
+            user_input = input("indtast din vaerdi : ")
+        if (user_input == 'ende' and langage == 'dk') or (user_input == 'fin' and langage == 'fr'):
             end_loop = 1
         elif user_input.isdigit() == True:
             user_input = float(user_input)
-            exec_prog(user_input)
+            exec_prog(user_input, langage)
     return 0
 
 def             main():
         if len(sys.argv) != 5:
-                print("Wrong number of parameters.")
+                print("Forkert antal parametre.\nMauvais nombre de paramètres.")
                 return 0
         global nbr_mesures, arith_avg, harmo_avg, ec_type
         nbr_mesures = float(sys.argv[1])
         arith_avg = float(sys.argv[2])
         harmo_avg = float(sys.argv[3])
         ec_type = float(sys.argv[4])
-        init()
+        langage_input = input("Vælg sprog :\nChoisissez votre langue : (dansk/français)\n")
+        if langage_input == 'français':
+            langage = 'fr'
+        elif langage_input == 'dansk':
+            langage = 'dk'
+        else:
+            print("Default language is \"dansk\"")
+            langage = 'dk'
+        init(langage)
         return 0
     
 main()
